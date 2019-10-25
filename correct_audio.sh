006ce4379ae032a2710953492323989d77b77075
Jon Barker <j.p.barker@sheffield.ac.uk>
2019-10-25 17:13:56 +0100

    Fixed typos in bash scripts

--- correct_audio.sh
+++ correct_audio.sh
@@ -1,15 +1,14 @@
-!#/bin/bash
+#!/bin/bash
 
 ### 
 
-source paths.sh  # Paths for CHiME5 and CHiME6
+source ./paths.sh  # Paths for CHiME5 and CHiME6
 
 ## Lines below load anaconda python on Sheffield HPC
-## and activate a conda environment supplying the
-## requirements in requirement.txt
-#
-# module load apps/python/anaconda3-2.5.0
-# source env3/bin/activate 
+## and activate a conda environment
+
+# module load apps/python/conda
+# source activate chime5
 
 ###
 
@@ -17,9 +16,9 @@ echo $session  # session variable passed in via qsub
 
 ###
 
-IN_PATH = $CHiME5_ROOT/audio    
-OUT_PATH = $CHiME6_ROOT/audio
-TMP_PATH = $CHiME6_ROOT/audio_tmp
+IN_PATH=$CHiME5_ROOT/audio    
+OUT_PATH=$CHiME6_ROOT/audio
+TMP_PATH=$CHiME6_ROOT/audio_tmp
 
 mkdir -p $OUT_PATH/train $OUT_PATH/eval $OUT_PATH/dev
 mkdir -p $TMP_PATH/train $TMP_PATH/eval $TMP_PATH/dev
@@ -28,4 +27,4 @@ mkdir -p $TMP_PATH/train $TMP_PATH/eval $TMP_PATH/dev
 python correct_signals_for_frame_drops.py --session=$session chime6_audio_edits.json $IN_PATH $TMP_PATH
 
 # Sox processing for correcting clock drift
-python correct_signals_for_clock_drift.py --session=$session  --sox_path $SOX_PATH chime6_audio_edits.json $TMP_PATH $OUT_PATH
+python correct_signals_for_clock_drift.py --session=$session --sox_path $SOX_PATH chime6_audio_edits.json $TMP_PATH $OUT_PATH
--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # Script to submit jobs for correcting alignment of all CHiME-5 audio
 
@@ -6,5 +6,15 @@ sessions="S01 S02 S03 S04 S05 S06 S07 S08 S09 S12 S13 S16 S17 S18 S19 S20 S21 S2
 
 for session in $sessions; do
     export session
-    qsub -v session correct_audio.sh
+    qsub -v session -j y correct_audio.sh
 done
+
+source ./paths.sh
+
+# module load apps/python/conda
+# source activate chime5
+
+
+mkdir -p $CHiME6_ROOT/transcriptions/eval  $CHiME6_ROOT/transcriptions/dev $CHiME6_ROOT/transcriptions/train
+
+python correct_transcript_for_clock_drift.py --clock_drift_data chime6_audio_edits.json $CHiME5_ROOT/transcriptions $CHiME6_ROOT/transcriptions
