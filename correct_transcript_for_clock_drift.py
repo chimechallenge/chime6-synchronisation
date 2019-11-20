6b4d29334a6d629e6a8349782f196907470b7987
Jon Barker <j.p.barker@sheffield.ac.uk>
2019-11-20 13:25:26 +0000

    added chime5 compatibility mode to correct_transcript_for_clock_drift.py

--- correct_transcript_for_clock_drift.py
+++ correct_transcript_for_clock_drift.py
@@ -20,8 +20,12 @@ CHIME_DATA = tu.chime_data()
 
 CHIME_SAMPLE_RATE = 16000  # CHiME sample rate.
 
-def correct_transcription_for_clock_drift(session, linear_fit_data, in_path, out_path):
-    """Apply clock drift to transcription file."""
+def correct_transcription_for_clock_drift(session, linear_fit_data, in_path, out_path, chime5_mode=False):
+    """Apply clock drift to transcription file.
+    
+    chime5_mode - if true then for each utterance write an (identical) start and end time for every device, 
+        otherwise use new, non-redundant chime6 format which only records the start and end time once
+    """
     dataset = CHIME_DATA[session]['dataset']
     transcription = tu.load_transcript(session, in_path + '/' + dataset, convert=True)
 
@@ -43,11 +47,22 @@ def correct_transcription_for_clock_drift(session, linear_fit_data, in_path, out
         # process transcription for each speaker
         for utterance in transcription:
             if utterance.get('speaker', None) == pid:
-                utterance['start_time'] = utterance['start_time']['original'] / speed + delta_t
-                utterance['end_time'] = utterance['end_time']['original'] / speed + delta_t
+                start_time = utterance['start_time']['original'] / speed + delta_t
+                end_time = utterance['end_time']['original'] / speed + delta_t
+                if chime5_mode:
+                    # Identical time entry for each device
+                    for key in utterance['start_time']:
+                        utterance['start_time'][key] = start_time
+                    for key in utterance['end_time']:
+                        utterance['end_time'][key] = end_time
+                else:
+                    # A single time entry 
+                    utterance['start_time'] = start_time
+                    utterance['end_time'] = end_time
 
     # Remove the speakerless segments that marked position of redactions. 
-    # There's no easy way to correct these timings without going back to the original transcripts.
+    # There's no easy way to correct these timings without going back to the original transcripts
+    # because the necessary speaker ids were removed as part of the redaction.
     transcription = [utterance for utterance in transcription if 'speaker' in utterance]
     tu.save_transcript(transcription, session, out_path + '/' + dataset, convert=True)
 
@@ -56,22 +71,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--sessions",
                         help="list of sessions to process (defaults to all)")
-    parser.add_argument("--clock_drift_data", help="json file storing clock drift data")   
+    parser.add_argument("--clock_drift_data", help="json file storing clock drift data")  
+    parser.add_argument('--chime5', action='store_true', help="write transcript file in older CHiME-5 format") 
     parser.add_argument("in_path", help="path for the input transcription file")
     parser.add_argument("out_path", help="path for the output transcription files")
+    
     args = parser.parse_args()
     if args.sessions is None:
         sessions = tu.chime_data()
     else:
         sessions = args.sessions.split()
 
+    print(args.chime5)
     with open(args.clock_drift_data) as f:
         linear_fit_data = json.load(f)
 
     for session in sessions:
         try:
             print(session)
-            correct_transcription_for_clock_drift(session, linear_fit_data, args.in_path, args.out_path)
+            correct_transcription_for_clock_drift(session, linear_fit_data, args.in_path, args.out_path, 
+                                                  chime5_mode=args.chime5)
         except:
             traceback.print_exc()
 
